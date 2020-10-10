2d93919e0550e3e77f71eb9e77315ddb92192f28
Michael Schantl <michael@schantl-lx.at>
2020-10-10 19:46:54 +0200

    Implement funcs for communication w/ systemd

---
+++ lib/systemd.sh
@@ -0,0 +1,15 @@
+
+sd_notify() {
+  if [[ $ARG_USE_SYSTEMD -ne 1 ]]; then
+    return
+  fi
+  systemd-notify $@
+}
+
+sd_notify_ready() {
+  sd_notify --ready
+}
+
+sd_notify_watchdog() {
+  sd_notify "WATCHDOG=1"
+}
